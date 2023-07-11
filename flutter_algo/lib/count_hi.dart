6f705b31505c47c07e6afae74be6d34f28b590bf
saurabh.yadav <saurabh.yadav@stspl.com>
2023-07-11 17:35:23 +0530

    first commit

---
+++ flutter_algo/lib/count_hi.dart
@@ -0,0 +1,17 @@
+void main() {
+  var count=countHi("chicccchi","h");
+  print("repeating $count");
+
+}
+
+int countHi(String str,String search) {
+  int pos = -1, count = -1;
+
+  do {
+    count++;
+    pos = str.indexOf(search, pos + 1);
+  } while (pos != -1);
+
+  return count;
+}
+
